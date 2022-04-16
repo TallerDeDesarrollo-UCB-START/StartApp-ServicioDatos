6be2360a154551e932e155791e973a8a811fd777
Jacovzap <jacovzap@gmail.com>
2022-04-15 23:25:09 -0400

    FInal test

--- config/pool.config.js
+++ config/pool.config.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 require('dotenv').config()
 
 const pool = new Pool({
-  user: "dzuskbzshbdkna",
-  password: "64a8ea20a16e3dc13445c55ae9c791bec47af524d3949783d7747e943b2fedf7", //use your pass my friend
-  database: "d7qd1m74q5go7f",
-  host: "ec2-54-158-26-89.compute-1.amazonaws.com",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, //use your pass my friend
+  database: process.env.DB_HOST,
+  host: process.env.DB_DATABASE,
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
