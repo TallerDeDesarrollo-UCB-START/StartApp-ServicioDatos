4516c1e79a3b35e02a165c501d5fbcc021061a38
Jacovzap <jacovzap@gmail.com>
2022-04-19 03:37:10 -0400

    Revert "last commit"
    This reverts commit 84b2a3caaaa23bc4e5a4c81848204b7f6aee04d7.

--- config/pool.config.js
+++ config/pool.config.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "dzuskshbdkna",
+  user: "dzuskbzshbdkna",
   password: "64a8ea20a16e3dc13445c55ae9c791bec47af524d3949783d7747e943b2fedf7", //use your pass my friend
   database: "d7qd1m74q5go7f",
   host: "ec2-54-158-26-89.compute-1.amazonaws.com",
