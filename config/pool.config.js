845d2206c73f585f82920d709d87c5f3dadfe373
AlvaroCori <bacs200018@gmail.com>
2022-04-23 19:22:40 -0400

    new database

--- config/pool.config.js
+++ config/pool.config.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "hsazteibnsnquc",
-  password: "96c44f19b6a31a67521c2fa65c9233544ed1d7d5388367c6d9ff4c22c940a340", //use your pass my friend
-  database: "d5mjf648gc2p7f",
-  host: "ec2-54-156-24-159.compute-1.amazonaws.com",
+  user: "pvsifhkgosgqog",
+  password: "afddd3387a8c58046f43d51b8a7bb0a110a5a940e6ad45afe210b4576ac4c9da", //use your pass my friend
+  database: "db389gkd6ou7j1",
+  host: "ec2-44-194-4-127.compute-1.amazonaws.com",
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
