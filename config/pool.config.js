0e3e808d399959cb0b8604ffcc66df3d565c048a
Jacovzap <jacovzap@gmail.com>
2022-05-05 01:16:59 -0400

    test 16

--- config/pool.config.js
+++ config/pool.config.js
@@ -2,18 +2,28 @@ const Pool = require("pg").Pool;
 
 
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_DATABASE);
+console.log(process.env.DB_DATABASE);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_PORT);
 
 
 const pool = new Pool({
-  user: "dzuskbzshbdkna",
-  password: "64a8ea20a16e3dc13445c55ae9c791bec47af524d3949783d7747e943b2fedf7", //use your pass my friend
-  database: "d7qd1m74q5go7f",
-  host: "ec2-54-158-26-89.compute-1.amazonaws.com",
-  port: 5432,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, //use your pass my friend
+    database: process.env.DB_HOST,
+    host: process.env.DB_DATABASE,
+    port: 5432,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
 
 module.exports.pool = pool;
 
@@ -23,6 +33,19 @@ module.exports.pool = pool;
 //   password: process.env.DB_PASSWORD, //use your pass my friend
 //   database: process.env.DB_HOST,
 //   host: process.env.DB_DATABASE,
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+
+
+// const pool = new Pool({
+//   user: "dzuskbzshbdkna",
+//   password: "64a8ea20a16e3dc13445c55ae9c791bec47af524d3949783d7747e943b2fedf7", //use your pass my friend
+//   database: "d7qd1m74q5go7f",
+//   host: "ec2-54-158-26-89.compute-1.amazonaws.com",
 //   port: 5432,
 //   ssl: {
 //     rejectUnauthorized: false,
