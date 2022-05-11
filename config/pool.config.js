eec1f23008a6be767b75c6f045566960c4a20e44
Jacovzap <jacovzap@gmail.com>
2022-05-11 02:54:56 -0400

    s-production6

--- config/pool.config.js
+++ config/pool.config.js
@@ -1,14 +1,40 @@
 const Pool = require("pg").Pool;
-
+require('dotenv').config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD, //use your pass my friend
-  database: process.env.DB_HOST,
-  host: process.env.DB_DATABASE,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 module.exports.pool = pool;
+
+// const pool = new Pool({
+//     user: "hsazteibnsnquc",
+//     password: "96c44f19b6a31a67521c2fa65c9233544ed1d7d5388367c6d9ff4c22c940a340", //use your pass my friend
+//     database: "d5mjf648gc2p7f",
+//     host: "ec2-54-156-24-159.compute-1.amazonaws.com",
+//     port: 5432,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+//   module.exports.pool = pool;
+  
+
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD, //use your pass my friend
+//   database: process.env.DB_HOST,
+//   host: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+// module.exports.pool = pool;
