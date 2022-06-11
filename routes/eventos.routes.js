7a8db05002a500b4e4193f1eadd5eacee7ada1ca
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2022-06-11 17:14:19 -0400

    Little change in the eventos.routes

--- routes/eventos.routes.js
+++ routes/eventos.routes.js
@@ -1,12 +1,12 @@
-const ServiceEvento = require("../services/eventoServicio");
-const Repository = require("../data/dbEventoRepositorio.js");
-const service_evento = new ServiceEvento(new Repository());
+const Service = require("../services/eventService");
+const repository = require("../data/dbEventRepository.js");
+const eventService = new Service(new repository());
 
 module.exports = function (app) {
   app.post("/eventos/crearevento", async (req, res) => {
     //Crear
     try {
-      const nuevoEvento = await service_evento.createEvent(req.body);
+      const nuevoEvento = await eventService.createEvent(req.body);
       res.status(201).json(req.body);
     } catch (error) {
       res.status(400).send(`{"message":"Cambios no fueron guardados, ${error.message}", "data":false}`);
@@ -15,7 +15,7 @@ module.exports = function (app) {
 
   app.get("/eventos", async (req, res) => {
     try {
-      const nuevoProyecto = await service_evento.getEvents(req);
+      const nuevoProyecto = await eventService.getEvents(req);
       res.status(200).json(nuevoProyecto.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo obtener los eventos, ${error.message}", "data":false}`);
@@ -24,7 +24,7 @@ module.exports = function (app) {
 
   app.get("/eventos/categorias", async (req, res) => {
     try {
-      const categorias = await service_evento.getCategories(req);
+      const categorias = await eventService.getCategories(req);
       res.status(200).json(categorias.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo obtener las categorias de eventos, ${error.message}", "data":false}`);
@@ -34,7 +34,7 @@ module.exports = function (app) {
   app.get("/eventos/participantes/:id", async (req, res) => {
     try {
       const participantes_eventos =
-        await service_evento.getAllParticipantsForEvent(req.params["id"]);
+        await eventService.getAllParticipantsForEvent(req.params["id"]);
       res.status(200).json(participantes_eventos.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo obtener los participantes del evento con id ${req.params["id"]}, ${error.message}", "data":false}`);
@@ -43,7 +43,7 @@ module.exports = function (app) {
 
   app.get("/eventos/:id", async (req, res) => {
     try {
-      const nuevoEvento = await service_evento.getEvent(req);
+      const nuevoEvento = await eventService.getEvent(req);
       res.status(200).json(nuevoEvento.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo obtener el evento con el id ${req.params["id"]}, ${error.message}", "data":false}`);
@@ -54,7 +54,7 @@ module.exports = function (app) {
     async (req, res) => {
       try {
         const { id, id_autenticacion } = req.params;
-        const evento_a_actualizar = await service_evento.participateEvent(
+        const evento_a_actualizar = await eventService.participateEvent(
           id,
           id_autenticacion
         );
@@ -68,7 +68,7 @@ module.exports = function (app) {
   app.delete("/eventos/:id", async (req, res) => {
     try {
       let { id } = req.params;
-      const eliminarEvento = await service_evento.deleteEvent(id, req.body);
+      const eliminarEvento = await eventService.deleteEvent(id, req.body);
       res.status(200).json(eliminarEvento.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo eliminar el evento con el id ${req.params["id"]}, ${error.message}", "data":false}`);
@@ -78,7 +78,7 @@ module.exports = function (app) {
   app.put("/eventos/archivar_evento/:id", async (req, res) => {
     try {
       let { id } = req.params;
-      const archivarEvento = await service_evento.updateStateEvent1(
+      const archivarEvento = await eventService.updateStateEvent1(
         id,
         req.body
       );
@@ -103,7 +103,7 @@ module.exports = function (app) {
   app.put("/eventos/mostrar_evento/:id", async (req, res) => {
     try {
       let { id } = req.params;
-      const archivarEvento = await service_evento.updateStateEvent2(
+      const archivarEvento = await eventService.updateStateEvent2(
         id,
         req.body
       );
@@ -115,7 +115,7 @@ module.exports = function (app) {
 
   app.get("/eventos/participante/:id", async (req, res) => {
     try {
-      const eventosDelUsuario = await service_evento.getEventsUser(
+      const eventosDelUsuario = await eventService.getEventsUser(
         req.params["id"]
       );
       res.status(200).json(eventosDelUsuario.rows);
@@ -126,7 +126,7 @@ module.exports = function (app) {
 
   app.get("/lideres", async (req, res) => {
     try {
-      const lideres = await service_evento.getLeaders(req);
+      const lideres = await eventService.getLeaders(req);
       res.status(200).json(lideres.rows);
     } catch (error) {
       res.status(404).send(`{"message":"No se pudo obtener los lideres, ${error.message}", "data":false}`);
@@ -139,7 +139,7 @@ module.exports = function (app) {
       try {
         const { idEvento, idUsuario } = req.params;
         const eliminarParticipacion =
-          await service_evento.deleteParticipationAnEvent(idEvento, idUsuario);
+          await eventService.deleteParticipationAnEvent(idEvento, idUsuario);
         res.status(200).json(eliminarParticipacion.rows);
       } catch (error) {
         res.status(404).send(`{"message":"No se pudo eliminar la participacion del usuari con id ${req.params["idUsuario"]}, ${error.message}", "data":false}`);
@@ -151,7 +151,7 @@ module.exports = function (app) {
   app.get("/sesion/:id_autenticacion/get_my_eventos", async (req, res) => {
     try {
       const { id_autenticacion } = req.params;
-      const mis_eventos = await service_evento.getMyEvents(id_autenticacion);
+      const mis_eventos = await eventService.getMyEvents(id_autenticacion);
       if (!mis_eventos)
         res
           .status(204)
@@ -172,7 +172,7 @@ module.exports = function (app) {
     try {
       let { id } = req.params;
       req.body["id"] = id;
-      const eventoActualizado = await service_evento.updateEvent(
+      const eventoActualizado = await eventService.updateEvent(
         id,
         req.body
       );
